a5548ce1ad51c2ca81abf5882226c880bf60fc27
MJR267-round <marlonnascimento1931@gmail.com>
2026-06-25 17:28:27 -0300

    Create jogos.js

---
+++ jogos.js
@@ -0,0 +1,36 @@
+// DADOS DE TODOS OS JOGOS
+const LISTA_JOGOS = [
+    {
+        id: "jogo1",
+        titulo: "🟡 BRASIL x ESTADOS UNIDOS | Estádio Nacional | 18/07 | 10:00 (UTC-3)",
+        timeA: "Brasil",
+        timeB: "EUA",
+        estadio: "Estádio Nacional",
+        data: "18/07",
+        horario: "10:00",
+        linkTransmissao: "",
+        salaVoz: "https://meet.jit.si/central-copa-jogo1"
+    },
+    {
+        id: "jogo2",
+        titulo: "🟢 ARGENTINA x MÉXICO | Arena Principal | 19/07 | 13:00 (UTC-3)",
+        timeA: "Argentina",
+        timeB: "México",
+        estadio: "Arena Principal",
+        data: "19/07",
+        horario: "13:00",
+        linkTransmissao: "",
+        salaVoz: "https://meet.jit.si/central-copa-jogo2"
+    },
+    {
+        id: "jogo3",
+        titulo: "🔵 FINAL DA COPA | Vencedor 1 x Vencedor 2 | Maracanã | 30/07 | 17:00 (UTC-3)",
+        timeA: "Vencedor Semifinal 1",
+        timeB: "Vencedor Semifinal 2",
+        estadio: "Maracanã",
+        data: "30/07",
+        horario: "17:00",
+        linkTransmissao: "",
+        salaVoz: "https://meet.jit.si/central-copa-jogo3"
+    }
+];
